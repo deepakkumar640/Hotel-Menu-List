4e7cdbd34836b362e2b2618c391742f59c8a1c0c
deepakkumar640 <95094333+deepakkumar640@users.noreply.github.com>
2022-06-05 23:13:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Restaurent from './components/basic/Restaurent'
+const App = () => {
+  return (
+    <>
+      <Restaurent></Restaurent>
+    </>
+  )
+}
+
+export default App
+
